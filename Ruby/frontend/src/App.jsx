a53aa74e59918d0b8bd9c8e7c5b0c3b7943a2c80
Fernanda <143466223+Sunref@users.noreply.github.com>
2025-11-20 21:01:15 -0300

    NEW: Frontend Vite + React + TailwindCSS
    And their respective *PARTIAL* configurations

---
+++ Ruby/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-900 mb-4">
+          Minha App
+        </h1>
+        <p className="text-gray-600">
+          Agora o conteúdo está centralizado no meio!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
